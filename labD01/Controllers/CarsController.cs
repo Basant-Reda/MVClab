c8e0e4ae9d692a54f12b3d7026cce1dd42cff0cf
basant <basantreda119@gmail.com>
2023-03-12 09:09:38 +0200

    Add project files.

---
+++ labD01/Controllers/CarsController.cs
@@ -0,0 +1,32 @@
+﻿using labD01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace labD01.Controllers
+{
+    public enum Status
+    {
+        list,
+        table
+    }
+    public class Data
+    {
+        public Car? car;
+        public Status? status;
+    }
+    public class CarsController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            var cars=Car.GetCars();
+            return View(cars);
+        }
+        public IActionResult GetDetailsForCar(string cModel , Status? status)
+        {
+            var car = Car.GetCars().FirstOrDefault(c => c.Model == cModel);
+            Data data= new Data();
+            data.car = car;
+            data.status = status;
+            return View(data);
+        }
+    }
+}
